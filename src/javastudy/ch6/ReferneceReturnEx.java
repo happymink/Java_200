f4ce4e2134fc6b06ade494c45c52f80f83291278
Minkyun <minkyun@kakao.com>
2022-02-28 23:10:55 +0900

    0228

---
+++ src/javastudy/ch6/ReferneceReturnEx.java
@@ -0,0 +1,7 @@
+package javastudy.ch6;
+
+public class ReferneceReturnEx {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/javastudy/ch6/test.java
@@ -0,0 +1,10 @@
+package javastudy.ch6;
+
+public class test {
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
