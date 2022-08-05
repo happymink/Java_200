b43264c18c2aaf92810a89ef18caaa6359767726
minkyun <minkyun@kakao.com>
2022-08-06 00:44:27 +0900

    Changes

---
+++ src/baekjoon/n10926.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class n10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ID = sc.nextLine();
+        System.out.println(ID + "??!");
+    }
+}
