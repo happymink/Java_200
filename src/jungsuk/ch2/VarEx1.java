daa71355a4ba057ce04cb09e13dce24c5ba8a3ed
minkyun <minkyun@kakao.com>
2023-11-20 19:18:36 +0900

    Changes

--- src/jungsuk/ch2/VarEx1.java
+++ src/jungsuk/ch2/VarEx1.java
@@ -1,15 +1,25 @@
 package jungsuk.ch2;
 
+import java.util.ArrayList;
+//추가 삭제 검색
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+
+        for(int i = 0; i>my_string.length(); i++){
+            System.out.println(my_string.length());
+            for(int j = 0; j >= n ; j++){
+                answer += my_string.substring(i,i+1);
+            }
+        }
+        return answer;
+    }
+}
 public class VarEx1 {
     public static void main(String[] args) {
-        int year = 0;
-        int age = 14;
-        System.out.println(year);
-        System.out.println(age);
-        year = age + 2000;
-        age = age + 1;
 
-        System.out.println(year);
-        System.out.println(age);
+        Solution a = new Solution();
+
+        System.out.println(a.solution("hello",3));
     }
 }
